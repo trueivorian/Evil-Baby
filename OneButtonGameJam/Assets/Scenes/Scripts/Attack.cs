b23f1e96332dbe1250a541aba635888761a8cdcc
henrytribble <60523630+henrytribble@users.noreply.github.com>
2020-09-25 21:08:26 +0100

    new project

---
+++ OneButtonGameJam/Assets/Scenes/Scripts/Attack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Health health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            health.Damage(20);
+            print(health.health);
+        }
+    }
+}
